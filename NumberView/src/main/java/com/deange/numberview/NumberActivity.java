0f6a97b329df28e40facbb6e51cd58e8cc590698
cdeange <christiand@carezone.com>
2013-11-18 01:10:33 -0800

    Created quick example for a counting mechanism

--- NumberView/src/main/java/com/deange/numberview/NumberActivity.java
+++ NumberView/src/main/java/com/deange/numberview/NumberActivity.java
@@ -3,11 +3,47 @@ package com.deange.numberview;
 import android.app.Activity;
 import android.os.Bundle;
 
+import java.util.Timer;
+import java.util.TimerTask;
+
 public class NumberActivity extends Activity {
+
+    private final Timer mTimer = new Timer();
+
+    private NumberView mTensView;
+    private NumberView mOnesView;
+
+    private int mTime = 0;
 
     @Override
     protected void onCreate(final Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mTensView = (NumberView) findViewById(R.id.number_tens_position);
+        mOnesView = (NumberView) findViewById(R.id.number_ones_position);
+
+        mTensView.setAutoAdvance(false);
+        mOnesView.setAutoAdvance(false);
+
+        mTensView.setSequence(new int[] { 0, 1, 2, 3, 4, 5 });
+
+        mTimer.scheduleAtFixedRate(new UpdateTask(), 0, 1000);
+    }
+
+    private void updateUi() {
+        mOnesView.advance();
+        if (mTime % 10 == 0) {
+            mTensView.advance();
+        }
+
+        mTime++;
+    }
+
+    private class UpdateTask extends TimerTask {
+        @Override
+        public void run() {
+            updateUi();
+        }
     }
 }
